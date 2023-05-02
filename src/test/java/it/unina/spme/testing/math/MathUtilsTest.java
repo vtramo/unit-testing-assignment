596a2176ba54c05dcf29c80159e35ecd7e4c2932
Vincenzo Tramo <vv.tramo@gmail.com>
2023-05-02 23:20:23 +0200

    task 1

--- src/test/java/it/unina/spme/testing/math/MathUtilsTest.java
+++ src/test/java/it/unina/spme/testing/math/MathUtilsTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MathUtilsTest {
     @Test
@@ -15,5 +17,24 @@ public class MathUtilsTest {
         List<Integer> divisors = MathUtils.getDivisors(8);
         List<Integer> expected = Arrays.asList(1,2,4,8);
         assertEquals(expected,divisors); // first attempt!
+    }
+
+    @Test
+    void testDivisorsOfEightWithoutHamcrest()  {
+        Set<Integer> expectedDivisors = Set.of(1, 2, 4, 8);
+
+        List<Integer> divisors = MathUtils.getDivisors(8);
+
+        if (expectedDivisors.size() != divisors.size()) fail();
+        for (Integer divisor: divisors)
+            if (!expectedDivisors.contains(divisor))
+                fail();
+    }
+
+    @Test
+    void testDivisorsOfEightWithHamcrest() {
+        List<Integer> divisors = MathUtils.getDivisors(8);
+
+        assertThat(divisors, containsInAnyOrder(1, 2, 4, 8));
     }
 }
